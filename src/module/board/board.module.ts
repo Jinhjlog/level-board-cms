5ea7f27e516cb5d931602981c7fbe1cf25d228dc
Jinhjlog <dndn2615@gmail.com>
2026-06-03 16:50:34 +0900

    🔧 module: DeletePostUseCase 등록 (FileUploadModule)

--- src/module/board/board.module.ts
+++ src/module/board/board.module.ts
@@ -27,14 +27,19 @@ import {
   UserLevelLookupServiceImpl,
 } from './infra/services';
 import { FindBoardListUseCase } from './application/usecases/find-board-list.usecase';
-import { FindPostListUseCase } from './application/usecases';
-import { FindPostDetailUseCase } from './application/usecases';
+import {
+  FindPostListUseCase,
+  FindPostDetailUseCase,
+  DeletePostUseCase,
+} from './application/usecases';
+import { FileUploadModule } from '../file-upload/file-upload.module';
 
 /**
  * 게시판 CMS BC (board).
- * G단계: FindBoardListUseCase(SPEC 4.5) 추가.
+ * G단계: FindBoardListUseCase(SPEC 4.5) 추가. DeletePostUseCase(SPEC 4.10) 추가.
  */
 @Module({
+  imports: [FileUploadModule],
   controllers: [
     AdminBoardController,
     BoardController,
@@ -52,6 +57,7 @@ import { FindPostDetailUseCase } from './application/usecases';
     FindPostListUseCase,
     FindPostDetailUseCase,
     FindAdminBoardListUseCase,
+    DeletePostUseCase,
   ],
 })
 export class BoardModule {}
